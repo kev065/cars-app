496d1d64ddd028fb95d60051e299c6f5a4509a3d
Levin Mwanganyi Mwambi <artmwanganyi09@outlook.com>
2023-11-09 22:05:08 +0300

    Now onto css

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,24 +1,41 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import { BrowserRouter as Router, Route, Routes, Link } from 'react-router-dom';
+import Home from './components/Home';
+import About from './components/About';
+import Cars from './components/Cars';
+import CarDetails from './components/CarDetails';
+import LeaseComponent from './components/Leasing';
+// import CarListing from './components/CarListing';
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <nav>
+        <ul>
+          <li>
+            <Link to="/">Home</Link>
+          </li>
+          <li>
+            <Link to="/about">About</Link>
+          </li>
+          <li>
+            <Link to="/cars">Cars</Link>
+          </li>
+          <li>
+            <Link to="/lease">Lease</Link>
+          </li>
+        </ul>
+      </nav>
+
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/cars" element={<Cars />} />
+        <Route path="/cars/:id" element={<CarDetails />} />
+        <Route path='/lease' element = {<LeaseComponent />} />
+      </Routes>
+    </Router>
   );
 }
 
---
+++ my-app/src/components/Cars.js
@@ -0,0 +1,66 @@
+import React, { useEffect, useState, useContext } from 'react';
+import { Link } from 'react-router-dom';
+import CartIcon from './CartIcon';
+import { CartContext } from './CartContext';
+import Search from './Search';
+
+function Cars() {
+  const [cars, setCars] = useState([]);
+  const [loading, setLoading] = useState(true);
+  const [cart, setCart] = useContext(CartContext);
+
+  useEffect(() => {
+    fetch('http://localhost:3000/Cars')
+      .then((response) => response.json())
+      .then((data) => {
+        setCars(data);
+        setLoading(false);
+      });
+  }, []);
+
+  if (loading) {
+    return <p>Loading...</p>;
+  }
+
+  const addToCart = (car) => {
+    setCart((prevCart) => [...prevCart, car]);
+  };
+
+  return (
+    <div className="cars-container">
+      <section className="content">
+        <CartIcon />
+        <Search cars={cars} addToCart={addToCart} />
+        <h2>Cars Page</h2>
+        <div className="card-container">
+          {cars.map((car) => (
+            <div key={car.id} className="card">
+              <img src={car.image} alt={`${car.make} ${car.brand}`} />
+              <h3>{car.make} {car.brand}</h3>
+              <p>Year: {car.yearOfMake}</p>
+              <p>Price: ${car.price}</p>
+              <p>Mileage: {car.mileage} miles</p>
+              <Link to={`/cars/${car.id}`}>View Details</Link>
+              <button onClick={() => addToCart(car)}>Add to Cart</button>
+              
+            </div>
+          ))}
+        </div>
+
+        
+        <div>
+          <h2>Your Cart</h2>
+          {cart.map((cartItem) => (
+            <div key={cartItem.id}>
+              <h3>{cartItem.make} {cartItem.brand}</h3>
+              <p>Year: {cartItem.year}</p>
+              <p>Price: ${cartItem.price}</p>
+            </div>
+          ))}
+        </div>
+      </section>
+    </div>
+  );
+}
+
+export default Cars;
